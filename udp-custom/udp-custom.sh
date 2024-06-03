4372b56ad5d9f46be29b6b815fd55dfe8441cc66
hokagelegend9999 <158546743+hokagelegend9999@users.noreply.github.com>
2024-06-03 13:24:51 +0700

    Update udp-custom.sh

--- udp-custom/udp-custom.sh
+++ udp-custom/udp-custom.sh
@@ -6,7 +6,7 @@ mkdir -p /etc/udp
 echo "change to time GMT+7"
 ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
 cd /etc/udp
-wget -q -O udp-custom "https://github.com/zhets/udp-custom/raw/main/udp-custom-linux-amd64"
+wget -q -O udp-custom "https://raw.githubusercontent.com/hokagelegend9999/alpha/main/udp-custom/udp-custom-linux-amd64"
 chmod +x udp-custom
 cd
 
