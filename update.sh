282e84c9007d1e83161ef122670e2eded2e71d1e
hokagelegend9999 <158546743+hokagelegend9999@users.noreply.github.com>
2024-06-03 13:10:57 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -27,13 +27,13 @@ fun_bar() {
     tput cnorm
 }
 res1() {
-    wget https://raw.githubusercontent.com/Tomketstore/aut1/main/menu/menu.zip
+    wget https://raw.githubusercontent.com/hokagelegend9999/alpha/main/menu/menu.zip
     unzip menu.zip
     chmod +x menu/*
     mv menu/* /usr/local/sbin
     rm -rf menu.zip
     rm -rf update.sh
-    wget -qO- fv-tunnel "https://raw.githubusercontent.com/Tomketstore/aut1/main/config/fv-tunnel" 
+    wget -qO- fv-tunnel "https://raw.githubusercontent.com/hokagelegend9999/alpha/main/config/fv-tunnel" 
     chmod +x fv-tunnel 
     bash fv-tunnel
     rm -rf fv-tunnel
