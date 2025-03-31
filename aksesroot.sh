abbaacb5b927439ebd363564525493e60549d93f
wahyuyaw <yawwahyu@gmail.com>
2025-03-31 17:40:12 +0700

    Update aksesroot.sh

--- aksesroot.sh
+++ aksesroot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # xixi
 # echo "$crot    ALL=(ALL:ALL) ALL" >> /etc/sudoers;
-wget -q -O /etc/ssh/sshd_config https://raw.githubusercontent.com/hokagelegend9999/alpha/refs/heads/main/sshd_config;
+wget -q -O /etc/ssh/sshd_config https://raw.githubusercontent.com/wahyuyaw/alpha/refs/heads/main/sshd_config;
 systemctl restart sshd;
 clear;
 echo -e "Masukkan Password:";
